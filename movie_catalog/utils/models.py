20bede4c9dee315f81840e0d7b3fc128f781fd3b
salimildere <salim.ildere@akinon.com>
2022-03-29 22:11:21 +0300

    Add StarterModel in utils.

---
+++ movie_catalog/utils/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class StarterModel(models.Model):
+    """
+    Base model for all models
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created_at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated_at"))
+
+    class Meta:
+        """
+        Meta class for all models
+        """
+
+        abstract = True
